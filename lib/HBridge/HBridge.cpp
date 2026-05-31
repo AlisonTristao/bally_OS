829337cb344862c5b842dd5d4ceddd73f20b9987
AlisonTristao <AlisonTristao@hotmail.com>
2026-05-31 15:39:25 -0300

    deadzone na ponte H

--- lib/HBridge/HBridge.cpp
+++ lib/HBridge/HBridge.cpp
@@ -1,57 +1,44 @@
 #include "HBridge.h"
 
-HBridge::HBridge(uint8_t l1, uint8_t l2, uint8_t channelPWM, uint8_t pinPWM)
-{
-    // define os pinos
-    this->l1 = l1;
-    this->l2 = l2;
-    this->channelPWM = channelPWM;
-    this->pinPWM = pinPWM;
-}
-void HBridge::init()
-{
-    // configura o pwm
+void HBridge::init() {
     ledcSetup(channelPWM, 5000, 12);
     ledcAttachPin(pinPWM, channelPWM);
-    // define a direcao
     mov = STOPPED;
 }
-void HBridge::changeDirection(movement move)
-{
-    // altera a rotação do motor
-    mov = move;
 
-    // define a rotação
-    switch (mov)
-    {
+void HBridge::changeDirection(movement move) {
+    mov = move;
+    switch (mov) {
     case FORWARD:
         digitalWrite(l1, HIGH);
         digitalWrite(l2, LOW);
         break;
-
     case BACKWARD:
         digitalWrite(l1, LOW);
         digitalWrite(l2, HIGH);
         break;
-
     case STOPPED:
         digitalWrite(l1, HIGH);
         digitalWrite(l2, HIGH);
         break;
-
     default:
         break;
     }
 }
-void HBridge::applyPWM(int32_t pwm)
-{
-    // limita o pwm
-    if (pwm > +100)
-        pwm = +100;
-    if (pwm < -100)
-        pwm = -100;
-    // normaliza o pwm (tira da porcentagem)
-    pwm = pwm * 40.95;
+
+int32_t HBridge::convertPWM(int32_t pwm) {
+    return (dead_zone + (pwm/SUPLIMIT) * (SUPLIMIT - dead_zone)) * gain;
+}
+
+int32_t HBridge::saturate(int32_t pwm) {
+    if (pwm > SUPLIMIT)
+        return SUPLIMIT;
+    if (pwm < INFLIMIT)
+        return INFLIMIT;
+    return pwm;
+}
+
+void HBridge::choiceDirection(int32_t pwm) {
     // define a direção
     movement move = FORWARD;
     if (pwm > 0)
@@ -62,13 +49,15 @@ void HBridge::applyPWM(int32_t pwm)
     // muda a direção
     if (mov != move)
         changeDirection(move);
-
-    // aplica o pwm
-    ledcWrite(channelPWM, abs(pwm));
 }
-void HBridge::brake()
-{
+
+void HBridge::applyPWM(int32_t pwm) {
+    choiceDirection(saturate(pwm));
+    ledcWrite(channelPWM, convertPWM(saturate(pwm)));
+}
+
+void HBridge::brake() {
     // para o motor
     changeDirection(STOPPED);
-    ledcWrite(channelPWM, 4095);
+    ledcWrite(channelPWM, convertPWM(100));
 }
--- lib/HBridge/HBridge.h
+++ lib/HBridge/HBridge.h
@@ -2,30 +2,37 @@
 #define WHEELS_H
 #include <Arduino.h>
 
+#define SUPLIMIT 100
+#define INFLIMIT -100
+
 // sentido da rotação das todas
-enum movement
-{
+enum movement {
     FORWARD,
     BACKWARD,
     STOPPED
 };
 
 // motor objeto
-class HBridge
-{
-public:
-    HBridge(uint8_t l1, uint8_t l2, uint8_t channelPWM, uint8_t pinPWM);
-    virtual ~HBridge(){};
-    void init();
-    void applyPWM(int32_t pwm);
-    void brake();
-private:
-    void changeDirection(movement mov);
-    movement mov;
-    uint8_t l1;
-    uint8_t l2;
-    uint8_t channelPWM;
-    uint8_t pinPWM;
+class HBridge {
+    public:
+        HBridge(uint8_t l1, uint8_t l2, uint8_t channelPWM, uint8_t pinPWM, uint8_t dead_zone = 0) : 
+                l1(l1), l2(l2), channelPWM(channelPWM), pinPWM(pinPWM), dead_zone(dead_zone) {};
+        virtual ~HBridge(){};
+        void init();
+        void applyPWM(int32_t pwm);
+        void brake();
+    private:
+        void changeDirection(movement mov);
+        void choiceDirection(int32_t pwm);
+        int32_t convertPWM(int32_t pwm);
+        int32_t saturate(int32_t pwm);
+        movement mov;
+        const uint8_t l1;
+        const uint8_t l2;
+        const uint8_t channelPWM;
+        const uint8_t pinPWM;
+        const uint8_t dead_zone;
+        const float gain = 40.95;
 };
 
 #endif
